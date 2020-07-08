c846e4cbbb85d71f62ef4df980e778479ad83912
Omer <lovemj353286116@qq.com>
2020-07-08 18:38:26 +0800

    Study for today

--- client.go
+++ client.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"rpc-go/lightweight"
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
+	"rpc-go/myFirstGrpcPackage"
 	"time"
 )
 
@@ -13,32 +13,50 @@ const (
 	address = "localhost:50051"
 )
 
+//rpc实例
+type goRpc struct {
+	rpc myFirstGrpcPackage.GymClient
+
+}
+
+//通过连接初始化Gym服务客户端
+func(g *goRpc) newGymClient(conn *grpc.ClientConn) {
+	g.rpc = myFirstGrpcPackage.NewGymClient(conn)
+}
+
+//普通rpc请求
+func(g *goRpc) commonRequest(ctx context.Context, request *myFirstGrpcPackage.RequestDemoOne) {
+	r, err := g.rpc.DemoOne(ctx, request)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("code: %d, msg: %s", r.Code, r.Msg)
+}
+
+// 服务端流rpc请求
+
+// 客户端流rpc请求
+
+// 双向流rpc请求
+
+
 func main() {
 	fmt.Print("rpc客户端启动")
-
-	// Set up a connection to the server.
+	// 建立客户端与服务端之间的grpc连接
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	c := lightweight.NewGymClient(conn)
+	gymClient:= goRpc{}
+	//通过连接初始化客户端 也可以封装
+	gymClient.newGymClient(conn)
+	// 设置请求超时时间
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	r, err := c.BodyBuilding(ctx, &lightweight.Person{
-		Name: "chenqionghe",
-		Actions: []string{"深蹲", "卧推", "硬拉"},
-	})
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	fmt.Printf("code: %d, msg: %s", r.Code, r.Msg)
-
-	t, err := c.DemoBuilding(ctx, &lightweight.RequestDemo{
-		Name: "guns n'r roses",
-	})
-	if err != nil {
-		log.Fatalf("error: %v", err)
-	}
-	fmt.Printf("code: %d, msg: %s", t.Code, t.Msg)
+	//普通rpc请求调用
+	//准备请求数据
+	commonRequest:= myFirstGrpcPackage.RequestDemoOne{Name:"Grumpy Omer"}
+	gymClient.commonRequest(ctx, &commonRequest)
 }
+
--- server.go
+++ server.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"rpc-go/lightweight"
+	"rpc-go/myFirstGrpcPackage"
 	"context"
 	"fmt"
 	"google.golang.org/grpc"
@@ -15,28 +15,43 @@ const (
 
 // server继承自动生成的服务类
 type server struct {
-	lightweight.UnimplementedGymServer
+	myFirstGrpcPackage.UnimplementedGymServer
 }
 
-// 服务端必须实现了相应的接口BodyBuilding
-func (s *server) BodyBuilding(ctx context.Context, in *lightweight.Person) (*lightweight.Reply, error) {
-	fmt.Printf("%s正在健身, 动作: %s\n", in.Name, in.Actions)
-	return &lightweight.Reply{Code: 0, Msg: "ok",}, nil
-}
-
-func (s *server) DemoBuilding(ctx context.Context, in *lightweight.RequestDemo) (*lightweight.ResponseDemo, error) {
+// 实现相应接口
+// 普通rpc方法
+func (s *server) DemoOne(ctx context.Context, in *myFirstGrpcPackage.RequestDemoOne) (*myFirstGrpcPackage.ResponseDemoOne, error) {
 	fmt.Println("there is a heaven above you! "+in.Name)
-	return &lightweight.ResponseDemo{Code: 200, Msg: "ok",}, nil
+	return &myFirstGrpcPackage.ResponseDemoOne{Code: 200, Msg: "ok",}, nil
 }
+
+// 服务端流rpc方法
+func (s *server) DemoTwo(rect *myFirstGrpcPackage.RequestDemoTwo, stream myFirstGrpcPackage.Gym_DemoTwoServer) error {
+	return nil
+}
+
+// 客户端流rpc方法
+func (s *server) DemoThree(stream myFirstGrpcPackage.Gym_DemoThreeServer) error {
+	return  nil
+}
+
+// 双向流rpc方法
+func (s *server) DemoFour(stream myFirstGrpcPackage.Gym_DemoFourServer) error {
+	return nil
+}
+
 func main() {
 	fmt.Print("rpc服务端启动")
+	//指定要监听客户端请求的端口
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
-
+	//创建一个gRPC server的实例
 	s := grpc.NewServer()
-	lightweight.RegisterGymServer(s, &server{})
+	//使用gRPC server注册服务实现
+	myFirstGrpcPackage.RegisterGymServer(s, &server{})
+
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
